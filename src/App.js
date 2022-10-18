43d20726f1aa52c1194bb0c9bce23fb8d7569966
isabellexvr <isabellexavier59@gmail.com>
2022-10-18 18:17:50 -0300

    style: login page static layout;

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import GlobalStyle from "./assets/css/GlobalStyle";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import styled from "styled-components";
+import HomePage from "./Pages/HomePage";
+import SignUpPage from "./Pages/SignUpPage";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage/>}/>
+        <Route path="/cadastro" element={<SignUpPage/>}/>
+      </Routes>
+      <GlobalStyle />
+    </BrowserRouter>
+  )
 }
 
-export default App;
---
+++ src/constants/colors.js
@@ -0,0 +1,6 @@
+const colors = {
+    blue: "#52B6FF",
+    grey: "#DBDBDB"
+}
+
+export default colors
